61d3372477fa406d75dee7160e6d2f66ca1f8c71
Sumanta Das <sumantad827@gmail.com>
2023-06-16 20:17:07 +0530

    initial commit

---
+++ python.py
@@ -0,0 +1,16 @@
+# replace all 0 with 5
+
+s = [1004,121]
+nes=[]
+
+for i in s:
+    data=""
+    for j in str(i):
+        if int(j)==0:
+            data+="5"
+        elif int(j)!=0:
+            data+=j  
+    nes.append(int(data))          
+for x in nes:
+    print(x)        
+
